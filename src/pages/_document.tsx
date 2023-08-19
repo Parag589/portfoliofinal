9f266135cb1ce69f2c094bd01e63328fdba5ee78
Parag Agarwal <paragagarwal0589@gmail.com>
2023-08-19 20:33:35 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,27 +12,35 @@ class IQDocument extends Document
         return (
             <Html data-theme='dark'>
                 <Head>
-                    <meta property='theme-color' content='#5294E2'/>
+                     <meta property='theme-color' content='#5294E2'/>
                     <meta name="robots" content="all" />
 
                     <meta name='language' content='en'/>
                     <meta name='Classification' content='Portfolio'/>
-                    <meta name='subject' content='IntelligentQuantum Full-Stack Developer & Graphic Designer'/>
-                    <meta name='description' content="IntelligentQuantum Full-Stack Developer & Graphic Designer. IntelligentQuantum focuses on JavaScript-related technologies like Typescript, NodeJS and React. IntelligentQuantum is also an experienced graphic designer and proficient in Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro and Gimp software's."/>
-                    <meta name='keywords' content='IntelligentQuantum, IntelligentQuantum Portfolio, Full-Stack Developer, Developer, Iran Shiraz, JavaScript, TypeScript, Graphic Designer'/>
-                    <meta name='author' content='IntelligentQuantum'/>
+                    <meta name='subject' content='Parag Agarwal Full-Stack Developer & Graphic Designer'/>
+                    <meta name='description' content="Parag Agarwal Full-Stack Developer & Graphic Designer. Parag Agarwal focuses on JavaScript-related technologies like Typescript, NodeJS and React. Parag Agarwal is also an experienced graphic designer and proficient in Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro and Gimp software's."/>
+                    <meta name='keywords' content='Parag Agarwal, Parag Agarwal Portfolio, Full-Stack Developer, Developer, Iran Shiraz, JavaScript, TypeScript, Graphic Designer'/>
+                    <meta name='author' content='Parag Agarwal'/>
 
                     <meta property='og:type' content='website'/>
-                    <meta property='og:url' content='https://intelligentquantum.dev/'/>
-                    <meta property='og:title' content='IntelligentQuantum'/>
-                    <meta property='og:description' content='IntelligentQuantum Full-Stack Developer & Graphic Designer'/>
-                    <meta property='og:image' content='https://intelligentquantum.dev/favicon.png'/>
+                    <meta property='og:url' content='https://paragportfolio.vercel.app/'/>
+                    <meta property='og:title' content='Parag Agarwal'/>
+                    <meta property='og:description' content='Parag Agarwal Full-Stack Developer & Graphic Designer'/>
+                    <meta property='og:image' content='https://paragportfolio.vercel.app/favicon.png'/>
 
                     <meta property='twitter:card'/>
-                    <meta property='twitter:url' content='https://intelligentquantum.dev/'/>
-                    <meta property='twitter:title' content='IntelligentQuantum'/>
-                    <meta property='twitter:description' content='IntelligentQuantum Full-Stack Developer & Graphic Designer'/>
-                    <meta property='twitter:image' content='https://intelligentquantum.dev/favicon.png'/>
+                    <meta property='twitter:url' content='https://paragportfolio.vercel.app/'/>
+                    <meta property='twitter:title' content='Parag Agarwal'/>
+                    <meta property='twitter:description' content='Parag Agarwal Full-Stack Developer & Graphic Designer'/>
+                    <meta property='twitter:image' content='https://paragportfolio.vercel.app/favicon.png'/>
+
+                    <link rel='icon' href='/favicon.png' />
+                    <link rel='apple-touch-icon' href='https://intelligentquantum.dev/favicon.png'/>
+                    <link rel='manifest' href='https://intelligentquantum.dev/manifest.json'/>
+                    <link rel='canonical' href='https://paragportfolio.vercel.app' />
+                    <link rel='preconnect' href='https://fonts.googleapis.com' />
+                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+                    <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet' />
 
                     <link rel='icon' href='/favicon.png' />
                     <link rel='apple-touch-icon' href='https://intelligentquantum.dev/favicon.png'/>
